a979d5053e5d6d85cbddae9b23663e1611e99193
Lorenzo Piazza <lollo96.piazza@gmail.com>
2018-06-13 11:07:33 +0200

    Restyling view Login e eliminazione file Server di prova

--- MyFantalega/Client/View/ViewLogin.Designer.cs
+++ MyFantalega/Client/View/ViewLogin.Designer.cs
@@ -47,7 +47,7 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.Font = new System.Drawing.Font("Lucida Bright", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label2.Location = new System.Drawing.Point(209, 6);
+            this.label2.Location = new System.Drawing.Point(186, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(440, 100);
             this.label2.TabIndex = 2;
@@ -57,24 +57,26 @@
             // 
             // tableLayoutPanel3
             // 
+            this.tableLayoutPanel3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.tableLayoutPanel3.ColumnCount = 2;
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45.17374F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 54.82626F));
             this.tableLayoutPanel3.Controls.Add(this.button3, 1, 0);
             this.tableLayoutPanel3.Controls.Add(this.button4, 1, 1);
             this.tableLayoutPanel3.Controls.Add(this.label3, 0, 1);
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(22, 316);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(77, 314);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 2;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(518, 100);
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(460, 100);
             this.tableLayoutPanel3.TabIndex = 1;
             // 
             // button3
             // 
             this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button3.Location = new System.Drawing.Point(321, 9);
+            this.button3.Location = new System.Drawing.Point(279, 9);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(108, 31);
             this.button3.TabIndex = 0;
@@ -85,7 +87,7 @@
             // button4
             // 
             this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button4.Location = new System.Drawing.Point(321, 58);
+            this.button4.Location = new System.Drawing.Point(279, 58);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(108, 33);
             this.button4.TabIndex = 1;
@@ -97,35 +99,36 @@
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(13, 65);
+            this.label3.Location = new System.Drawing.Point(42, 55);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(207, 20);
+            this.label3.Size = new System.Drawing.Size(122, 40);
             this.label3.TabIndex = 4;
             this.label3.Text = "Non sei ancora registrato?";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // tableLayoutPanel4
             // 
+            this.tableLayoutPanel4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.tableLayoutPanel4.ColumnCount = 1;
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 76.10442F));
             this.tableLayoutPanel4.Controls.Add(this.textBoxUsername, 0, 1);
             this.tableLayoutPanel4.Controls.Add(this.label4, 0, 0);
             this.tableLayoutPanel4.Controls.Add(this.textBoxPassword, 0, 3);
             this.tableLayoutPanel4.Controls.Add(this.label5, 0, 2);
-            this.tableLayoutPanel4.Location = new System.Drawing.Point(118, 109);
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(150, 112);
             this.tableLayoutPanel4.Name = "tableLayoutPanel4";
             this.tableLayoutPanel4.RowCount = 4;
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 64F));
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 51F));
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
-            this.tableLayoutPanel4.Size = new System.Drawing.Size(572, 184);
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(514, 184);
             this.tableLayoutPanel4.TabIndex = 0;
             // 
             // textBoxUsername
             // 
             this.textBoxUsername.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBoxUsername.Location = new System.Drawing.Point(82, 67);
+            this.textBoxUsername.Location = new System.Drawing.Point(53, 67);
             this.textBoxUsername.Name = "textBoxUsername";
             this.textBoxUsername.Size = new System.Drawing.Size(407, 22);
             this.textBoxUsername.TabIndex = 0;
@@ -135,7 +138,7 @@
             // 
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(202, 11);
+            this.label4.Location = new System.Drawing.Point(173, 11);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(168, 42);
             this.label4.TabIndex = 1;
@@ -146,7 +149,7 @@
             // textBoxPassword
             // 
             this.textBoxPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBoxPassword.Location = new System.Drawing.Point(85, 152);
+            this.textBoxPassword.Location = new System.Drawing.Point(56, 152);
             this.textBoxPassword.MaxLength = 20;
             this.textBoxPassword.Name = "textBoxPassword";
             this.textBoxPassword.PasswordChar = '*';
@@ -157,7 +160,7 @@
             // 
             this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(202, 96);
+            this.label5.Location = new System.Drawing.Point(173, 96);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(168, 42);
             this.label5.TabIndex = 3;
@@ -169,6 +172,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.tableLayoutPanel3);
