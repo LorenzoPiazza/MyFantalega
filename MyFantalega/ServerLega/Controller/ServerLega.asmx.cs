f398f65ca7a8c9d0b5659eca017b058c2fd7b790
Jacopo Ingraito <jacopo.ingraito@outlook.it>
2018-06-24 20:19:59 +0200

    serveLega

--- MyFantalega/ServerLega/Controller/ServerLega.asmx.cs
+++ MyFantalega/ServerLega/Controller/ServerLega.asmx.cs
@@ -20,34 +20,89 @@ namespace ServerLega.Controller
     public class ServerLega : System.Web.Services.WebService
     {
 
+        //PARTECIPA ASTA CONTROLLER
         [WebMethod]
-        public Lega CreaLega(String nome, int numeroPartecipanti, Utente utente)
-        {
-            IGestioneUtenteController gestioneUtenteController = new GestioneUtenteController();
-            Lega lega = gestioneUtenteController.CreaLega(nome, numeroPartecipanti, utente);
-            return lega;
-        }
-
         public Boolean AbbandonaGiocatore(Asta asta, Squadra squadra)
         {
             IPartecipaAstaController partecipaAstaController = new PartecipaAstaController();
             return partecipaAstaController.AbbandonaGiocatore(asta, squadra);
         }
 
+        [WebMethod]
         public Boolean RialzaOfferta(Asta asta, Squadra squadra)
         {
             IPartecipaAstaController partecipaAstaController = new PartecipaAstaController();
             return partecipaAstaController.RialzaOfferta(asta, squadra);
         }
+
+
+        [WebMethod]
+        public Boolean OffriCrediti(int offerta, Asta asta, Squadra squadra)
+        {
+            IPartecipaAstaController partecipaAstaController = new PartecipaAstaController();
+            return partecipaAstaController.OffriCrediti(offerta, asta, squadra);
+        }
+
+        //GESTIONE UTENTE CONTROLLER
+        [WebMethod]
+        public Lega CreaLega(String nome, int numeroPartecipanti, Utente utente)
+        {
+            IGestioneUtenteController gestioneUtenteController = new GestioneUtenteController();
+            return gestioneUtenteController.CreaLega(nome, numeroPartecipanti, utente);
+        }
+
+        [WebMethod]
+        public Boolean cambiaPassword(String vecchiaPass, String nuovaPass, String domanda, String risposta, Utente utente)
+        {
+            IGestioneUtenteController gestioneUtenteController = new GestioneUtenteController();
+            return gestioneUtenteController.cambiaPassword(vecchiaPass, nuovaPass, domanda, risposta, utente);
+        }
+
+        [WebMethod]
+        public List<Lega> getLeghe(Utente utente)
+        {
+            IGestioneUtenteController gestioneUtenteController = new GestioneUtenteController();
+            return gestioneUtenteController.getLeghe(utente);
+        }
+
+        [WebMethod]
+        public Boolean uniscitiAMercato(Mercato mercato, Squadra squadra)
+        {
+            IGestioneUtenteController gestioneUtenteController = new GestioneUtenteController();
+            return gestioneUtenteController.uniscitiAMercato(mercato, squadra);
+        }
+
+        //GESTIONE SVINCOLO CONTROLLER
+        [WebMethod]
+        public Boolean SvincolaGiocatore(Giocatore g, Squadra s)
+        {
+            IGestioneSvincoloController gestioneSvincoloController = new GestioneSvincoloController();
+            return gestioneSvincoloController.SvincolaGiocatore(g, s);
+        }
+
+        //GESTIONE MERCATO CONTROLLER
+        [WebMethod]
+        public Boolean ChiudiMercato(Mercato mercato)
+        {
+            IGestioneMercatoController gestioneMercatoController = new GestioneMercatoController();
+            return gestioneMercatoController.ChiudiMercato(mercato);
+        }
+
+        //GESTIONE LEGA CONTROLLER
+        [WebMethod]
+        public Boolean SetImpostazioni(int sqTot, int creIni, int numPor, int numDif, int numCen, int numAtt, Lega lega)
+        {
+            IGestioneLegaController gestioneLegaController = new GestioneLegaController();
+            return gestioneLegaController.SetImpostazioni(sqTot, creIni, numPor, numDif, numCen, numAtt, lega);
+        }
+
+        [WebMethod]
+        public Boolean CaricaLista(String source, Lega lega)
+        {
+            IGestioneLegaController gestioneLegaController = new GestioneLegaController();
+            return gestioneLegaController.CaricaLista(source, lega);
+        }
     }
 }
 
-        [WebMethod]
-
-        public Boolean CaricaLista(String source, Lega lega)
-        {
-            ICaricaListaController caricaListaController = new CaricaListaFileController();
-            return caricaListaController.CaricaLista(source, lega);
-        }
-    }
-
+        
